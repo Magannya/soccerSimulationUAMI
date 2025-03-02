9ea8387af3944cc8303cce6928f23272233dcfe1
Gustavo Magana <gustavomagana903@gmail.com>
2025-03-02 16:26:29 -0600

    print_manager-0.1

--- src/Print_manager.py
+++ src/Print_manager.py
@@ -6,7 +6,8 @@ class Print_manager:
 	
 	REFRESH_INTERVAL = 0.1
 	DINAMIC_ON_SCREEN_TIME = 3
-	staticQueue = ""
+	
+	staticQueue = []
 	dinamicQueue = []
 	screenTimeQueue = []
 	lastRefresh = 0
@@ -23,7 +24,54 @@ class Print_manager:
 	
 	def getRefreshInterval(self):
 		return self.refreshInterval
+		
+	def setStaticQueue(self, s):
+		self.staticQueue = s;
+		
+	def getStaticQueue(self):
+		return self.staticQueue
+		
+	def setDinamicQueue(self, lst):
+		self.dinamicQueue = lst
+		
+	def getDinamicQueue(self):
+		return self.dinamicQueue
+		
+	def setScreenTimeQueue(self, lst):
+		self.screenTimeQueue = lst
 	
-	def dPrintAppend(message):
-		self.dinamicQueue.append(message)
+	def getScreenTimeQueue(self):
+		return self.screenTimeQueue
+	
+	# MAIN--------------------------------------------------------------
+	
+	def dPrintAppend(self, s):
+		self.dinamicQueue.append(s)
 		self.screenTimeQueue.append(time.time())
+		
+	def sPrintAppend(self, s):
+		self.staticQueue.append(s)
+		
+	def refresh(self):
+		now = time.time()
+		if now > self.lastRefresh + self.REFRESH_INTERVAL:
+			os.system('clear')
+			self.printSQ()
+			self.printDQ(now)
+			self.lastRefresh = now
+	
+	# COMPLEMENTARY-----------------------------------------------------
+	
+	def printSQ(self):
+		for s in self.staticQueue:
+			print(s)
+			
+	def printDQ(self, now):
+		i = 0
+		for s in self.dinamicQueue:
+			if now - self.screenTimeQueue[i] < self.DINAMIC_ON_SCREEN_TIME:
+				print(s)
+			else:
+				del self.dinamicQueue[i]
+				del self.screenTimeQueue[i]
+			i += 1
